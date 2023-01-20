4c77d2500df422363d095afe7b1aecdd32b172e8
BharaT-Coderr <bharatsarkar66666@gmail.com>
2023-01-20 21:20:07 +0530

    searching and showing user from database is done

--- lib/utilis/global_variables.dart
+++ lib/utilis/global_variables.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/Screen/add_post_screen.dart';
 import 'package:instagram_clone/Screen/feed_screen.dart';
+import 'package:instagram_clone/Screen/search_screen.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 const webScreenSize = 600;
 
 List<Widget> homeScreenItems = [
   const FeedScreen(),
-  "search".text.xl.makeCentered(),
+  const SearchScreen(),
   const AddPostScreen(),
   "fav".text.xl.makeCentered(),
   "pro".text.xl.makeCentered(),
